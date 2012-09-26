2065968d7d1a9997ecd27f232df07949d62fb7f8
Aaylor <loloruna@orange.fr>
2012-09-26 17:03:16 +0200

    add main

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main{
+
+    /**
+     *  Command line interface
+     *  @param args     The command line parameters.
+     */
+
+    /**
+     *  Function to start the program
+     */
+    public static void start_program(){
+
+    }
+
+    public static void main(String[] args){
+
+        /*  add here some function/variable before starting program     */
+
+        start_program();
+
+    }
+
+}
