44dfb1c24fcb31fccafb6ba1295b9b1687393a79
BlisterBoy <mehdi.abelha@gmail.com>
2012-10-05 10:29:53 +0300

    Update src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,16 @@ public class Main{
     }
 
     /**
+    * Fonction de vérification du fichier du configuration
+    * Renvoie true si le fichier respecte les normes précisées dans la documentation
+    * Renvoie false dans le cas contraire, et affiche un message précisant que les parametre par défaut seront chargé
+    */
+    
+    public static boolean verifFichierConfig(){
+    
+    }
+    
+    /**
      *  Fonction main
      *  @param args     Paramètres des lignes de commandes
      */
