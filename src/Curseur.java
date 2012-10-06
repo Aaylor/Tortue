e207f63709e9b2ffc2286366b7c2caed734d31d8
BlisterBoy <mehdi.abelha@gmail.com>
2012-10-06 13:09:20 +0300

    Update src/Curseur.java
    Passage des attributs du curseur en static

--- src/Curseur.java
+++ src/Curseur.java
@@ -1,10 +1,10 @@
 import java.awt.Color;
 
 public class Curseur {
-	private int x, y; //Coordonnées du curseur
-	private int orientation; //Orientation du curseur en degré (de 0 à 360);
-	private int type; //Il s'agit du matériel du curseur (0 = curseur, 1 = Gomme);
-	private Color couleur;
+	private static int x, y; //Coordonnï¿½es du curseur
+	private static int orientation; //Orientation du curseur en degrï¿½ (de 0 ï¿½ 360);
+	private static int type; //Il s'agit du matï¿½riel du curseur (0 = curseur, 1 = Gomme);
+	private static Color couleur;
 	
 	Curseur(){
 		
@@ -31,11 +31,11 @@ public class Curseur {
 	
 	//////////MODIFIEURS
 	
-	void setX(int x){
-		this.x = x;
+	void setX(int a){
+		x = a;
 	}
-	void setY(int y){
-		this.y = y;
+	void setY(int a){
+		y = a;
 	}
 	void setOrientation(int o){
 		orientation = o;
