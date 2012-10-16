4783a4be076c5a856e7c130acd9f68b84b71698b
Aaylor <loloruna@orange.fr>
2012-10-16 16:32:52 +0200

    change

--- src/ZoneBouton.java
+++ src/ZoneBouton.java
@@ -11,26 +11,18 @@ public class ZoneBouton extends JPanel {
      *  Constructeur de la zone de bouton
      */
 	ZoneBouton(){
-		this.setLayout(new GridLayout(2,2));
-		
-<<<<<<< HEAD
-		this.add(new JButton("test1"));
-		this.add(new JButton("test2"));
-		this.add(new JButton("test3"));
-		this.add(new JButton("test4"));
-=======
 		
         this.setLayout(new BorderLayout());
 	    JPanel grid_button = new JPanel();
-        grid_button.setLayout(new GridLayout());
+        grid_button.setLayout(new GridLayout(2,2));
         this.add(grid_button);
 
         grid_button.add(new JButton("test1"));
 		grid_button.add(new JButton("test2"));
 		grid_button.add(new JButton("test3"));
 		grid_button.add(new JButton("test4"));
->>>>>>> parent of 969fb07... fix du positionnement
-	}
+	
+    }
 
     /**
      *  Modifieur du controleur
