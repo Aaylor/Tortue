c4f64129c089538eaf5438613546832a3ded6b39
fredericmamath <contact@fredericmamath.fr>
2012-10-17 20:07:29 +0300

    Update src/Curseur.java
    >__> putin c'est de la merde github

--- src/Curseur.java
+++ src/Curseur.java
@@ -1,114 +1,195 @@
 import java.awt.Color;
+import java.awt.Graphics;
+import java.lang.Math;
 
 public class Curseur {
+	/////////////////
+	//  VARIABLES  //
+	/////////////////
+	private double x;
+	private double y;
+	private double orientation;
+	private int taille;
+	private short type;
+	private Color couleur;
+	private Controleur controleur;
 
-    private static int x, y; //Coordonnees du curseur
-	private static int orientation; //Orientation du curseur en degre (de 0 � 360);
-	private static short type; //Il s'agit du mat�riel du curseur (0 = curseur, 1 = Gomme);
-	private static Color couleur;
-    private Controleur controleur;
-
+	
+	/////////////////////
+	//  CONSTRUCTEURS  //
+	/////////////////////
     /**
      *  Constructeur vide
      */
-	Curseur(){
-		
+	Curseur () {
+
+	}
+	
+	/**
+	 * Constructeur test
+	 */
+	Curseur (int x, int y, int orientation, Color couleur) {
+		this.x = x;
+		this.y = y;
+		this.orientation = orientation;
+		this.taille = 10;
+		this.couleur = couleur;
+		this.type = 0;
+		this.controleur = null;
 	}
 	
 	
-	//////////ACCESSEURS
-
+	//////////////////
+	//  ACCESSEURS  //
+	//////////////////
     /**
-     *  Accesseur de la variable int x
+     *  Accesseur de x
      *  @return x
      */
-	int getX(){
-		return x;
+	public double getX () {
+		return this.x;
 	}
 
     /**
-     *  Accesseur de la variable int y
+     *  Accesseur de y
      *  @return y
      */
-	int getY(){
-		return y;
+	public double getY () {
+		return this.y;
 	}
 
     /**
      *  Accesseur de la variable orientation
      *  @return orientation
      */
-	int getOrientation(){
-		return orientation;
+	public double getOrientation () {
+		return this.orientation;
+	}
+	
+	/**
+	 * Accesseur de la variable taille
+	 * @return taille
+	 */
+	public int getTaille () {
+		return this.taille;
 	}
 
     /**
-     *  Accesseur de la variable short type
+     *  Accesseur de la variable type
      *  @return type
      */
-	short getType(){
-		return type;
+	public short getType () {
+		return this.type;
 	}
 
     /**
-     *  Accesseur de la variable Color couleur
+     *  Accesseur de la variable couleur
      *  @return couleur
      */
-	Color getCouleur(){
-		return couleur;
+	public Color getCouleur () {
+		return this.couleur;
 	}
 	
-	//////////MODIFIEURS
+	/////////////////
+	//  MODIFIEURS //
+	/////////////////
+    /**
+     *  Modifieur de la variable x
+     *  @param x nouvelle valeur de la position x
+     */
+	public void setX (double x) {
+		this.x = x;
+	}
+
+    /**
+     *  Modifieur de la variable y
+     *  @param y nouvelle valeur de la position y
+     */
+	public void setY (double y) {
+		this.y = y;
+	}
+
+    /**
+     *  Modifieur de la variable orientation
+     *  @param orientation nouvelle valeur de  l'orientation
+     */
+	public void setOrientation (double orientation) {
+		this.orientation = orientation;
+	}
+	
+	/**
+	 * Modifieur de la variable taille
+	 * @param taille nouvelle valeur de la taille
+	 */
+	public void setTaille (int taille) {
+		this.taille = taille;
+	}
 	
     /**
-     *  Modifieur de la variable int x
-     *  @param a nouvelle valeure de x
+     *  Modifieur de la variable type
+     *  @param type nouvelle valeur du type
      */
-	void setX(int a){
-		x = a;
+	public void setType (short type) {
+		this.type = type;
 	}
 
     /**
-     *  Modifieur de la variable int y
-     *  @param a nouvelle valeure de y
+     *  Modifieur de la variable couleur
+     *  @param couleur nouvelle valeur de couleur
      */
-	void setY(int a){
-		y = a;
-	}
-
-    /**
-     *  Modifieur de la variable int orientation
-     *  @param o nouvelle valeure d'orientation
-     */
-	void setOrientation(int o){
-		orientation = o;
-	}
-
-    /**
-     *  Modifieur de la variable short type
-     *  @param t nouvelle valeure du type
-     */
-	void setType(short t){
-		type = t;
-	}
-
-    /**
-     *  Modifieur de la variable Color couleur
-     *  @param c nouvelle valeure de couleur
-     */
-	void setCouleur(Color c){
-		couleur = c;
+	public void setCouleur (Color couleur) {
+		this.couleur = couleur;
 	}
 
     /**
      *  Modifieur du controleur
-     *  @param c nouveau controleur
+     *  @param controleur nouveau controleur
      */
-    void setControleur(Controleur c)
+    public void setControleur(Controleur controleur)
     {
-        this.controleur = c;
+        this.controleur = controleur;
     }
 	
-	//////////METHODES
-
+    ///////////////
+    //  METHODES //
+    ///////////////  
+    
+    /**
+     * Converti une valeur radian en degree
+     * @param degree
+     * @return radian
+     */
+    public double convertToRadian (double degree) {
+    	double radian;
+    	
+    	radian = Math.toRadians(degree);
+    	
+    	return radian;
+    }
+    
+    /**
+     * Donne la valeur du cosinus a partir d'une valeur en radian
+     * @param radian
+     * @return cos
+     */
+    public double calculCos (double radian) {
+    	double cos;
+    	
+    	cos = Math.cos(radian);
+    	
+    	return cos;
+    }
+    
+    /**
+     * Donne la valeur du sinus a partir d'une valeur en radian
+     * @param radian
+     * @return sin
+     */
+     public double calculSin (double radian) {
+    	 double sin;
+    	 
+    	 sin = Math.cos(radian);
+    	 
+    	 return sin;
+     }
 }
