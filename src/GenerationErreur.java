80fa34475c5ce7ca7e29f1b8342126a26aea350e
Aaylor <loloruna@orange.fr>
2012-10-17 18:29:17 +0200

    Début des générations d'erreurs (#TODO : Ajout d'un HashTable avec les nums d'erreurs correspondant);

--- src/GenerationErreur.java
+++ src/GenerationErreur.java
@@ -1,7 +1,16 @@
 public class GenerationErreur{
 
-    public static void GenerationErreur()
+    public static String genererErreur(int numero_erreur)
     {
+
+        String message_erreur = "Erreur " + numero_erreur + " : ";
+
+        if ( numero_erreur == 1 )
+        {
+            return message_erreur + "la commande n'existe pas.";
+        }
+
+        return "";
 
     }
 
