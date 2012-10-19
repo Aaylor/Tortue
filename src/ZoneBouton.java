3a9adc10cb5612a55250237ed6357591c66165be
BlisterBoy <mehdi.abelha@gmail.com>
2012-10-19 14:16:12 +0200

    Bouton de la zone bouton cliquable

--- src/ZoneBouton.java
+++ src/ZoneBouton.java
@@ -1,11 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class ZoneBouton extends JPanel {
-
     private Controleur controleur;
-
-
+    private boolean crayonEstPose = true; //TEMPORAIRE, juste pour l'initialisation du bouton localement
+    private boolean gomme;//False : Crayon, True : Gomme
+    private JButton boutonPoserCrayon = boutonPoserCrayon();
+    private JButton boutonGomme = boutonGomme();
+    
     /**
      *  Constructeur de la zone de bouton
      */
@@ -16,11 +19,68 @@ public class ZoneBouton extends JPanel {
 		
 		//Ajout des boutons
 		Box b = Box.createHorizontalBox();
-        b.add(new JButton("Lever le crayon"));
-        b.add(new JButton("Gomme"));
+		b.add(boutonPoserCrayon);
+		b.add(boutonGomme);
         this.add(b);
+		
+		//Action des boutons
+		boutonPoserCrayon.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg0){
+				if (crayonEstPose){
+					boutonPoserCrayon.setText("Poser le crayon");
+					crayonEstPose = false;
+				}
+				else{
+					boutonPoserCrayon.setText("Lever le Crayon");
+					crayonEstPose = true;
+				}
+			}
+		});
+		boutonGomme.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg0){
+				if (gomme){
+					boutonGomme.setText("Gomme");
+					gomme = false;
+				}
+				else{
+					boutonGomme.setText("Crayon");
+					gomme = true;
+				}
+			}
+		});
+        
+        
         
     }
+	
+	 /**
+	  * Fonction renvoyant le Bouton Lever/Poser le Crayon
+	  */
+	public JButton boutonPoserCrayon(){
+		JButton bouton = new JButton();
+		//Texte contenu dans le bouton
+		if (crayonEstPose) bouton.setText("Lever le crayon");
+		else bouton.setText("Poser le Crayon");
+		
+		//Return du bouton
+		return bouton;
+	}
+	
+	
+	 /**
+	  * Fonction renvoyant le Bouton Crayon/Gomme
+	  */
+	public JButton boutonGomme(){
+		JButton bouton = new JButton();
+		//Texte contenu dans le bouton
+		if (gomme) bouton.setText("Crayon");
+		else bouton.setText("Gomme");
+		
+		//Return du bouton
+		return bouton;
+	}
+	
+	
     /**
      *  Modifieur du controleur
      *  @param c nouveau controleur
