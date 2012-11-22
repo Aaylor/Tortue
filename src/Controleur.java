e42497c5df5b0f4dcbc3ab0db8a0e831c7dc79bf
kaizoku-kuma <lo.gauthier.glo@gmail.com>
2012-11-22 18:17:04 +0100

    remodif open

--- src/Controleur.java
+++ src/Controleur.java
@@ -1113,20 +1113,27 @@ public class Controleur{
      */
     public int open(String path)
     {
-    	/*verifier que le fichiet existe*/
     	String[] path_tab=path.split(".");
-    	if(path_tab[path_tab.length-1]=="png"){
-	        ImageIcon img=new ImageIcon(path);
-	    	int imageHeight=img.getIconHeight();
-	    	int imageWidth=img.getIconWidth();
-	    	if(imageHeight>zd.getHauteurDessin()){
-	    		zd.setHauteur(imageHeight);
-	    	}
-	    	if(imageWidth>zd.getLargeurDessin()){
-	    		zd.setLargeur(imageWidth);
-	    	}
-	    	Traceur t = new Traceur(5,path);
-        
+    	File file=new File(path);
+    	
+    	if(path_tab[path_tab.length-1]=="png"	//on test d'abord que l'extension est bien en png  
+    	|| path_tab[path_tab.length-1]=="jpg"	//ou jpg
+    	|| path_tab[path_tab.length-1]=="gif"	//ou gif
+    	|| path_tab[path_tab.length-1]=="jpeg"	//ou jpeg
+    	){ 
+    		if(file.exists()){ //si l'extension est bonne on vérifie l'extence du fichier
+		        ImageIcon img=new ImageIcon(path);
+		    	int imageHeight=img.getIconHeight();
+		    	int imageWidth=img.getIconWidth();
+		    	
+		    	if(imageHeight>zd.getHauteurDessin()){//on resize la zone en si le dessin est plus grand
+		    		zd.setHauteur(imageHeight);
+		    	}
+		    	if(imageWidth>zd.getLargeurDessin()){//on resize la zone en si le dessin est plus grand
+		    		zd.setLargeur(imageWidth);
+		    	}
+		    	Traceur t = new Traceur(5,path);
+    		}
     	}
     	else{
     		//message erreur
