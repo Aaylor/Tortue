ea6c02f721e6b9a20e91192b4ff726ae78d00b18
Aaylor <loic.runarvot@gmail.com>
2012-12-14 12:02:46 +0100

    javadoc

--- src/com/utilitary/Memory.java
+++ src/com/utilitary/Memory.java
@@ -7,6 +7,10 @@ public class Memory
     private boolean already_created = false;
     private int compteur;
 
+    /**
+     *  Constructeur de la mémoire
+     *  @param length Taille maximum de la mémoire
+     */
     public Memory( int length )
     {
         memory = new int[length];
@@ -22,36 +26,67 @@ public class Memory
         compteur = 0;
     }
 
+    /**
+     *  Réaffecte la valeur de la mémoire à l'index indiqué
+     *  @param index Index où la mémoire doit être changé.
+     *  @param value Valeur a insérer.
+     */
     public void set( int index, int value )
     {
         memory[index] = value;
     }
 
+    /**
+     *  Retourne la valeur de la mémoire à l'index indiqué
+     *  @param index Index de la mémoire à récupérer
+     *  @return La valeur entière
+     */
     public int get( int index )
     {
         return memory[index];
     }
 
+    /**
+     *  Retourne la taille de la mémoire
+     *  @return La taille
+     */
     public int length()
     {
         return memory.length;
     }
 
-    public void setCreation( boolean b )
+    /**
+     *  Indique si la mémoire a déjà été créée
+     *  @param b Boolean indiquant l'état de la création
+     */
+    private void setCreation( boolean b )
     {
        already_created = b; 
     }
 
+    /**
+     *  Renvoie l'état de la création de la mémoire
+     *  @return Etat
+     */
     public boolean getCreation()
     {
         return already_created;
     }
 
+    /**
+     *  Relace le compteur à l'endroit indiqué
+     *  @param new_compteur Nouvel index du compteur
+     */
     public void setCompteur(int new_compteur)
     {
         compteur = new_compteur;
     }
 
+    /**
+     *  Incrémente le compteur selon la disposition des index minimums et maximums
+     *  @param c_min Index minimum
+     *  @param c_max Index maximum
+     */
     public void incrementCompteur(int c_min, int c_max)
     {
         compteur++;
@@ -61,6 +96,10 @@ public class Memory
         }
     }
 
+    /**
+     *  Retour l'index du compteur
+     *  @return index
+     */
     public int getCompteur()
     {
         return compteur;
